fcf5c893443311e2945d97dbe7fe0a91ea481a87
Gal Sasson <45914262+GalSasson6@users.noreply.github.com>
2025-11-28 11:50:00 +0200

    added setbpm

--- include/AudioTrack.h
+++ include/AudioTrack.h
@@ -105,6 +105,9 @@ public:
     // ========== ACCESSOR FUNCTIONS ==========
     std::string get_title() const { return title; }
     int get_bpm() const { return bpm; }
-    int get_duration() const { return duration_seconds; }
+    int get_duration() const { return duration_seconds; }   
     std::vector<std::string> get_artists() const { return artists; }
+    void set_bpm(int new_bpm) { bpm = new_bpm; }
+
+
 };
